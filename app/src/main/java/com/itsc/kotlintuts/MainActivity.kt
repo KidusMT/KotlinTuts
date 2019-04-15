c055acb9db5e3c6728c2dcc67f0a2bc958cf44dc
Kidus Tekeste <kidusmamuye@gmail.com>
2019-04-15 11:37:27 +0300

    added toast in main activity

--- app/src/main/java/com/itsc/kotlintuts/MainActivity.kt
+++ app/src/main/java/com/itsc/kotlintuts/MainActivity.kt
@@ -2,11 +2,14 @@ package com.itsc.kotlintuts
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this,"working",Toast.LENGTH_SHORT).show()
     }
 }
